804c3486c55bf52df30f4adb9ca09c9b44d46c4d
Ryan <ryanmavilia@gmail.com>
2014-01-01 01:51:08 -0500

    Happy New Years

--- linx.py
+++ linx.py
@@ -1,9 +1,10 @@
 import socket
 import plugins
 import imp
+from datetime import datetime
 
 server = "squid.awfulnet.org" #Server
-channel = "#programming" #Channel
+channel = "#teenagers" #Channel
 nickname = "Linx" #Bot nickname
 password = raw_input("What is your password? ")
 port = 6667
@@ -14,11 +15,12 @@ ircsocket.send("NICK "+ nickname+"\r\n")
 ircsocket.send("USER "+nickname+" 0 * "+ ":RYAN"+"\r\n")
 archiveFile = open("archive.txt", 'a+')
 loop=True
+
 while loop:
 	incomingMsg = ircsocket.recv (4096) #Make Data the Receive Buffer
 	print incomingMsg #Print the Data to the console(For debug purposes)
-	archiveFile.write(incomingMsg)#log it in the archive txt doc
-	if "reload" in incomingMsg:
+	archiveFile.write(str(datetime.now().strftime('%Y/%m/%d %H:%M:%S'))+" "+ incomingMsg)#log it in the archive txt doc
+	if "|reload" in incomingMsg:
 		imp.reload(plugins)
 		myPlugins=plugins.PluginsClass(server,channel,nickname,password,ircsocket)
 	myPlugins.pluginStatements(ircsocket,incomingMsg)
--- plugins.py
+++ plugins.py
@@ -1,4 +1,6 @@
+
 class PluginsClass:
+
 	def __init__(self,newServer,newChannel,newNickname,newPassword,newSocket):
 		self.channel=newChannel
 		self.server=newServer
@@ -7,26 +9,32 @@ class PluginsClass:
 		self.ircsocket=newSocket
 	def sendMessage(self,ircsocket,newChannel,message):
 		ircsocket.send("PRIVMSG "+newChannel+" :"+message+"\r\n")
+	def sendAction(self,ircsocket,newChannel,message):
+		ircsocket.send("ACTION "+newChannel+" :"+message+"\r\n")
 	def joinChannel(self,ircsocket,newChannel):
-		ircsocket.send("JOIN " + newChannel+"\r\n")
+		ircsocket.send("JOIN " + newChannel+"\r\ning")
 	def leaveChannel(self,ircsocket,newChannel,leaveMessage):
 		ircsocket.send("PART "+newChannel+" "+leaveMessage+"\r\n")
 	def pluginStatements(self,ircsocket,incomingMsg):
-		if incomingMsg.find('PING') != -1: #If PING is Found in the Data
+		if incomingMsg.find('PING') != -1: #If PING is Found in the data send a pong back
 			ircsocket.send('PONG '+incomingMsg.strip("PING "))
 		if "376" in incomingMsg:
 			ircsocket.send("JOIN " + self.channel+"\r\n")
 		if "366" in incomingMsg:
 			ircsocket.send("PRIVMSG NickServ :identify "+self.nickname+" "+self.password+"\r\n")
 		if "licks" in incomingMsg:
-			ircsocket.send("PRIVMSG "+self.channel+" :STOP LICKING THE NICE KITTY!\r\n")
-		if "#bio" in incomingMsg:
-			self.sendMessage("the lick bot.")
-		if "#join" in incomingMsg:
-			newChannel=incomingMsg[(incomingMsg.index("#join ")+6):]
+			ircsocket.send("PRIVMSG "+self.channel+" :that was hot.\r\n")
+		if "|bio" in incomingMsg:
+			self.sendMessage(ircsocket,self.channel,"created by Mav type |help for other commands.")
+		if "|help" in incomingMsg:
+			self.sendMessage(ircsocket,self.channel,"Current commands: |help (fuck you im working on it)")	
+		if "|join" in incomingMsg:
+			newChannel=incomingMsg[(incomingMsg.index("|join ")+6):]
 			self.joinChannel(ircsocket,newChannel)
-		if "#kill" in incomingMsg:
+		if "|kill" in incomingMsg:
 			loop=False
-		if "#YOLO" in incomingMsg:
+		if "|slap" in incomingMsg:
+			self.sendAction(ircsocket,self.channel,"slaps "+incomingMsg.split()[3]+" with a large trout")
+		if "|YOLO" in incomingMsg:
 			self.sendMessage(ircsocket,incomingMsg.split()[2],"FUCK IT! IM OUTIE DOE")
 			self.leaveChannel(ircsocket,incomingMsg.split()[2], "Said fuck it, yolo.")
