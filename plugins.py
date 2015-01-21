accb66342644022de3288f56f4a62c782a660c50
Carter Hinsley <carterhinsley@gmail.com>
2015-01-21 01:21:21 -0500

    replace incorrect `if`s with `elif`s
    this should fix issues with messages that contain multiple triggering commands enacting multiple actions.

--- plugins.py
+++ plugins.py
@@ -22,29 +22,29 @@ class Plugins:
 		if incoming_msg.startswith("PING") #If message is a PING message send a PONG in response
 			irc_socket.send("PONG %s" % incoming_msg[5:])
 			
-		if "001" in incoming_msg:
+		elif "001" in incoming_msg:
 			self.send_message(irc_socket, "NickServ", "identify %s %s" % (self.nickname, self.password))
 			irc_socket.send("JOIN %s\r\n" % self.channel)
 			
-		if "licks" in incoming_msg:
+		elif "licks" in incoming_msg:
 			self.send_message(irc_socket, current_channel, "that was hot.")
 			
-		if "|bio" in incoming_msg:
+		elif "|bio" in incoming_msg:
 			self.send_message(irc_socket, current_channel, "created by Mav; type |help for other commands.")
 			
-		if "|help" in incoming_msg:
+		elif "|help" in incoming_msg:
 			self.send_message(irc_socket, current_channel, "Current commands: |help DICKS (fuck you im working on it)")
 			
-		if "|join" in incoming_msg:
+		elif "|join" in incoming_msg:
 			new_channel = incoming_msg[(incoming_msg.index("|join ")+6):]
 			self.join_channel(irc_socket, new_channel)
 		
-		if "|kill" in incoming_msg:
+		elif "|kill" in incoming_msg:
 			loop = False
 			
-		if "|slap" in incoming_msg:
+		elif "|slap" in incoming_msg:
 			self.send_action(irc_socket, self.channel, "slaps %s with a large trout" % incoming_msg.split()[3])
 			
-		if "|YOLO" in incoming_msg:
+		elif "|YOLO" in incoming_msg:
 			self.send_message(irc_socket, incoming_msg.split()[2], "FUCK IT! IM OUTIE DOE")
 			self.leave_channel(irc_socket, incoming_msg.split()[2], "Said fuck it, yolo.")
