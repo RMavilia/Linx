1f4f96c27b7a5704225eed6dc9e8dc21a402e117
Ryan Mavilia <ryanmavilia@gmail.com>
2017-05-15 13:01:04 -0400

    Spacing issue

--- linx.py
+++ linx.py
@@ -8,7 +8,7 @@ server = raw_input("Server address? ") #Server
 channel = raw_input("What channel? ") #Channel
 nickname = raw_input("Nickname? ") or "Linx" #Bot nickname
 password = raw_input("What is your password? ") #Password Input
-port = raw_input("What is the port? (Press ENTER if 6667)") or 6667
+port = raw_input("What is the port? (Press ENTER if 6667) ") or 6667
 
 
 irc_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
